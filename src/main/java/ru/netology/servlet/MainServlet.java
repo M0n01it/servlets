cf10740b469a51180f91269a5600dbfa71720af0
Nikolay Golovin <iimonolitii@mail.ru>
2025-01-12 23:40:39 +0300

    add annotation-based configuration

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,10 +1,13 @@
 package ru.netology.servlet;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.controller.PostController;
+import ru.netology.config.AppConfig;
 import ru.netology.exception.NotFoundException;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,17 +22,19 @@ public class MainServlet extends HttpServlet {
   private static final Pattern ID_PATTERN = Pattern.compile("/api/posts/(\\d+)");
 
   @Override
-  public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
+  public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+    // Initialize Spring Application Context
+    ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    // Retrieve PostController bean
+    controller = context.getBean(PostController.class);
   }
 
   @Override
   protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     try {
-      final var path = req.getRequestURI();
-      final var method = req.getMethod();
+      var path = req.getRequestURI();
+      var method = req.getMethod();
 
       switch (method) {
         case "GET":
